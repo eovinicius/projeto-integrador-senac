8412b2bfa9153095d7ae48293dd7f807a8e37857
vini <voliveirasantosss@gmail.com>
2023-08-30 10:54:56 -0300

    validacao do status no createcontroller

--- src/app/controllers/Course/CreateCourseController.ts
+++ src/app/controllers/Course/CreateCourseController.ts
@@ -1,11 +1,21 @@
 import { Request, Response } from 'express';
 import { prisma } from '../../../repositories/prismaCliente';
 import { AppError } from '../../middlewares/Error/AppError';
+import { Status } from '@prisma/client';
+
+interface ICreateCourse {
+  name: string;
+  description: string;
+  estatus: Status;
+}
 
 export class CreateCourseController {
   static async handle(req: Request, res: Response): Promise<Response> {
-    const { name, description, estatus } = req.body;
+    const { name, description, estatus }: ICreateCourse = req.body;
 
+    if (estatus != 'active' && estatus != 'inactive') {
+      throw new AppError(400, 'status errado!');
+    }
     if (!name || !description || !estatus) {
       throw new AppError(400, 'preencha todos os campos!');
     }
