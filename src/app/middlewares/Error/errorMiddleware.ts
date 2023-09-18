59cda5635e5a31806df17ef28cf3cbaa9fab7e22
vini <voliveirasantosss@gmail.com>
2023-09-18 11:16:03 -0300

    update errorMiddleware

--- src/app/middlewares/Error/errorMiddleware.ts
+++ src/app/middlewares/Error/errorMiddleware.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import { AppError } from './AppError';
+import { ZodError } from 'zod';
 
 export const errorMiddleware = (error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({ status: 'Error', message: error.message });
+  }
+
+  if (error instanceof ZodError) {
+    return res.status(400).json({ status: 'validation error', message: error.format() });
   }
 
   console.log(error.message);
