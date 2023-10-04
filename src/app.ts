08c20f64b26e3593374ce9e722b1965c7cfafa0e
vini <voliveirasantosss@gmail.com>
2023-10-04 09:15:47 -0300

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import 'express-async-errors';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import { errorMiddleware } from './app/middlewares/Error/errorMiddleware';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(routes);
 
